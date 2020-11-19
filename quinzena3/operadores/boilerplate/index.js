a96204bd56f1751a366d1a537e257b469d4d142e
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2020-11-18 22:24:10 -0300

    Exercícios Operadores, sem o desafio

---
+++ quinzena3/operadores/boilerplate/index.js
@@ -0,0 +1,64 @@
+// 1.
+// a. true
+// b. true
+// c. true
+// e. boolean
+
+// 2.
+// a. undefined
+// b. null
+// c. 11
+// d. 3
+// e. 3 19 5 6 7 8 9 10 11 12 13
+// f. 10
+
+// Escrita de código:
+
+// 1.
+// let suaIdade = Number(prompt("Qual é a sua idade"))
+// let idadeAmigo = Number(prompt("Qual é a idade do(a) seu(sua) melhor amigo(a)?"))
+
+// let maiorIdade = suaIdade > idadeAmigo
+
+// console.log("Sua idade é maior do que a do seu melhor amigo?", maiorIdade)
+
+// let diferencaIdade = suaIdade - idadeAmigo
+// console.log("A diferença entre as idades é de ", diferencaIdade, " anos")
+
+
+// 2.
+// let numeroPar = Number(prompt("Por favor, insira um número par:"))
+// let resto = numeroPar%2
+// console.log("O resto deste número divido por 2 é: ", resto)
+// o resto da divisão dos números pares por 2, é sempre 0
+
+// se o usuário inserir um número impar, o resto da divisão por 2 será sempre 1
+
+
+// 3.
+// let listaDeTarefas = []
+
+// listaDeTarefas.push(prompt("Quais tarefas você precisa realizar hoje?"))
+// listaDeTarefas.push(prompt("Quais tarefas você precisa realizar hoje?"))
+// listaDeTarefas.push(prompt("Quais tarefas você precisa realizar hoje?"))
+
+// console.log(listaDeTarefas)
+
+// let indice = prompt("Digite o índice da sua tarefa (0,1 ou 2):")
+
+// listaDeTarefas.splice(indice, 1)
+
+// console.log(listaDeTarefas)
+
+
+// 4.
+// const nomeDoUsuario = prompt("Qual é o seu nome?")
+// const emailDoUsuario = prompt("Qual é o seu e-mail?")
+
+// console.log("O e-mail ", emailDoUsuario," foi cadastrado com sucesso. Seja bem-vinda(o), ", nomeDoUsuario,"!")
+
+
+
+
+
+
