d828ff30b5863c5262f4b8922564fbcdabb6c86b
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2020-11-23 22:43:13 -0300

    condicionais sem desafio

---
+++ quinzena3/condicionais/boilerplate/index.js
@@ -0,0 +1,68 @@
+// EXERCÍCIO 1
+// o código testa se o resto da divisão numero digitado pelo usuário por 2 é igual a 0
+// para numeros pares ou que tem o resto da divisão do número digitado pelo usuário por 2 é igual a 0 imprime "Passou no teste?"
+// para numeros impares ou que tem resto da divisão diferente de 0 a mensagem mostrada é "Não passou no teste"?  
+
+// EXERCÍCIO 2
+// a. Para verificar o preço de cada fruta
+// b. O preço da fruta Maçã é R$ 2.25
+// c. O preço da fruta Pêra é R$ 5
+
+// EXERCÍCIO 3
+// a. declarando uma variavel com nome numero, que recebe um prompt, este por sua vez está tendo seu retorno transformado no tipo number
+// b. para entrada 10: Esse número passou no teste e também erro de variavel mensagem não definida
+//    para entrada -10: erro de variavel mensagem não definida
+// c. Sim. Ocorre o erro da variável mensagem não ter sido definida, devido ela ter sido declarada dentro do escopo if.
+
+// Exercícios de escrita de código
+
+// EXERCÍCIO 4
+// let idadeDoUsuario = Number(prompt("Qual é a sua idade"))
+
+// if(idadeDoUsuario >= 18){
+//     console.log("Você pode dirigir")
+// }else{
+//     console.log("Você não pode dirigir")
+// }
+
+// EXERCÍCIO 5
+// const turnoAluno = prompt("Qual turno você estuda? Digite M(matutino) ou V (Vespertino) ou N (Noturno)")
+
+// if(turnoAluno === "M"){
+//     console.log("Bom dia!")
+// }else if(turnoAluno === "V"){
+//     console.log("Boa tarde!")
+// }else{
+//     console.log("Boa noite!")
+// }
+
+// EXERCÍCIO 6
+
+// const turnoAluno = prompt("Qual turno você estuda? Digite M(matutino) ou V (Vespertino) ou N (Noturno)")
+
+// switch(turnoAluno){
+//     case "M":
+//         console.log("Bom dia!")
+//     break
+//     case "V":
+//         console.log("Boa tarde!")
+//     break
+//     case "N":
+//         console.log("Boa noite!")
+//     break
+// }
+
+
+// EXERCÍCIO 7
+// const generoFilme = prompt("Qual é o gênero do Filme?")
+// const precoIngresso = prompt("Qual o preço do Ingresso?")
+
+// if(generoFilme === "fantasia" && precoIngresso <= "15"){
+//     console.log("Bom Filme!")
+// }else{
+//     console.log("Escolha outro Filme :( ")
+// }
+
+
+
+
