d9260689a94d8f7e087ec385f0baae5c6a2e82a7
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2020-12-07 21:21:29 -0300

    salvar para continuar depois

---
+++ quinzena4/lista-de-exercicios/index.js
@@ -0,0 +1,14 @@
+// Exercícios de interpretação de código
+// o que faz; como faz; qual o valor impresso
+
+// 1. 
+// Converte o valor de 100 dolares em reais
+// pega os 100 do invocador 'conversorDeMoeda', 
+// pede a cotação do dolar para o usuário, 
+// multiplica a cotação pelo valor que pegou do invocador e retorna o valor com o R$ na frente
+// o retorno é atribuido a variável meuDinheiro
+// é impresso no console o valor da variavel meuDinheiro
+
+// 2.
+// 
+console.log("hellow node")
