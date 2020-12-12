b6c1819a66a4fe0cf97a0163ab155d340909a347
ThiagodaSilva27 <thiagotwoseven@gmail.com>
2020-12-12 03:21:26 -0300

    Exercícios de Funções de Array

--- quinzena4/lista-de-exercicios/index.js
+++ quinzena4/lista-de-exercicios/index.js
@@ -1,4 +1,4 @@
-// ---  Exercícios de interpretação de código  ---
+// ---  Exercícios de INTERPRETAÇÃO DE CÓDIGO  ---
 
 // 1. Converte um valor de dolar para real
 // chama a função com o valor 100, 
@@ -25,7 +25,7 @@
 
 
 
-// ---  Exercícios de Lógica de Programação  ---
+// ---  Exercícios de LÓGICA DE PROGRAMAÇÃO  ---
 
 // 1.
 // const array = [50, 30, 20, 10, 33, 27, 73, 100, 1920, 3500]
@@ -128,7 +128,7 @@
 
 
 
-//  ---  Exercícios de Funções  ---
+//  ---  Exercícios de FUNÇÕES  ---
 // 1.
 // const array = [50, 30, 20, 10, 33, 27, 73, 100, 1920, 7000]
 
@@ -160,7 +160,7 @@
 
 
 
-// ---  Exercícios de Objetos  ---
+// ---  Exercícios de OBJETOS  ---
 // 1. Quando temos uma lista de coisas que possuem características
 // específicas é necessário utilizar um array de objetos, 
 // como exemplo: o cadastro de um cliente em uma loja de ecommerce
@@ -211,3 +211,152 @@
 // }
 
 // anonimizarPessoa(pessoa)
+
+
+
+
+// ---  Exercícios de FUNÇOES DE ARRAY  ---
+// 1.
+// a e b
+// let pessoas = [
+//     {nome: "Pedro", idade: 20},
+//     {nome: "João" , idade: 10},
+//     {nome: "Paula", idade: 12},
+//     {nome: "Artur", idade: 89}
+// ]
+
+// let arrayAdultos = []
+// let arrayCriancas = []
+
+// pessoas.forEach((pessoa)=>{
+//     if(pessoa.idade >= 20){
+//         arrayAdultos.push(pessoa.nome)
+//     }
+//     else{
+//         arrayCriancas.push(pessoa.nome)
+//     }
+// })
+
+// console.log(`As pessoas adultas são: ${arrayAdultos}`)
+// console.log(`O nome das crianças são ${arrayCriancas}`)
+
+// 2.
+// const array = [1, 2, 3, 4, 5, 6]
+// a)
+// array.forEach((dobro)=>{
+//     dobro *= 2
+//     console.log(dobro)
+// })
+
+
+// b)
+// let mudaParaString = ""
+// array.forEach((triploString)=>{
+//     triploString *= 3
+//     mudaParaString = String(triploString)
+//     console.log(mudaParaString)
+// })
+
+
+// c)
+// let mudaParaString = ""
+// array.forEach((parOuImpar)=>{
+//     if(parOuImpar % 2 === 0){
+//         mudaParaString = String(parOuImpar)
+//         console.log(`${mudaParaString} é par `)
+//     }else{
+//         mudaParaString = String(parOuImpar)
+//         console.log(`${mudaParaString} é impar `)
+//     }
+// })
+
+
+// 3.
+// const pessoas = [
+// 	{ nome: "Paula", idade: 12, altura: 1.8},
+// 	{ nome: "João", idade: 20, altura: 1.3},
+// 	{ nome: "Pedro", idade: 15, altura: 1.9},
+// 	{ nome: "Luciano", idade: 22, altura: 1.8},
+// 	{ nome: "Artur", idade: 10, altura: 1.2},
+// 	{ nome: "Soter", idade: 70, altura: 1.9}
+// ]
+
+// let pessoasPermitidas = []
+
+// pessoas.forEach((pessoa)=>{
+//     if(pessoa.altura >= 1.5 && pessoa.idade > 14 && pessoa.idade < 60){
+//         pessoasPermitidas.push(pessoa.nome)
+//     }
+    
+// })
+
+// console.log(`Pessoas que podem entrar no brinquedo: ${pessoasPermitidas}`)
+
+// //b)
+// let pessoasNaoPermitidas = []
+
+// pessoas.forEach((pessoa)=>{
+//     if(pessoa.altura < 1.5 || pessoa.idade <= 14 || pessoa.idade >= 60){
+//         pessoasNaoPermitidas.push(pessoa.nome)
+//     }
+// })
+
+// console.log(`Pessoas que Não podem entrar no brinquedo: ${pessoasNaoPermitidas}`)
+
+
+
+// 4.
+// const consultas = [
+// 	{ nome: "João", genero: "masculino", cancelada: true, dataDaConsulta: "01/10/2019" },
+// 	{ nome: "Pedro", genero: "masculino", cancelada: false, dataDaConsulta: "02/10/2019" },
+// 	{ nome: "Paula", genero: "feminino", cancelada: true, dataDaConsulta: "03/11/2019" },
+// 	{ nome: "Márcia", genero: "feminino", cancelada: false, dataDaConsulta: "04/11/2019" }
+// ]
+
+// consultas.forEach((pessoa)=>{
+//     if(pessoa.cancelada === false){
+//         if(pessoa.genero === "masculino"){
+
+//             console.log(`Olá, Sr. ${pessoa.nome}. Estamos enviando esta mensagem para
+//             lembrá-lo da sua consulta no dia ${pessoa.dataDaConsulta}. Por favor, acuse
+//             o recebimento deste e-mail.`)
+
+//         }else{
+//             console.log(`Olá, Sra. ${pessoa.nome}. Estamos enviando esta mensagem para
+//             lembrá-la da sua consulta no dia ${pessoa.dataDaConsulta}. Por favor, acuse
+//             o recebimento deste e-mail.`)
+//         }
+//     }else{
+//         if(pessoa.genero === "masculino"){
+
+//             console.log(`Olá, Sr. ${pessoa.nome}. Infelizmente, sua consulta marcada
+//             para o dia ${pessoa.dataDaConsulta} foi cancelada. Se quiser, pode entrar em 
+//             contato conosco para remarcá-la`)
+
+//         }else{
+//             console.log(`Olá, Sra. ${pessoa.nome}. Infelizmente, sua consulta marcada
+//             para o dia ${pessoa.dataDaConsulta} foi cancelada. Se quiser, pode entrar em 
+//             contato conosco para remarcá-la`)
+//         }
+//     }
+// })
+
+
+
+// 5.
+const contas = [
+	{ cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+
+contas.forEach((saldo)=>{
+    saldo.compras.forEach((valor)=>{
+        saldo.saldoTotal -= valor
+    })
+})
+
+console.log(contas)
